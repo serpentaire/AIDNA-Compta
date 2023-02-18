927cee47506bd084f93ec85fc31a768dc370b61c
serpentaire <glemoine@hotmail.fr>
2023-02-18 11:11:19 +0100

    Création du footer et install Tailwind

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '@assets/logo.png'
+
+function Footer() {
+  return (
+    <div className="footer pt-10 front-roboto">
+      <div className="pb-4 flex justify-center">
+        <img src={logo} alt="logo" className="w-20 h-7 text-center" />
+        <p className="pl-2">Copyright - 2023 - Tous droits réservés</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="footer">
+    <div className="footer text-center">
       <h1>Home</h1>
     </div>
   );
