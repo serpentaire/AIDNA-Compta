acef444344b66a04b9166e9fbc2764ee0a3de5b7
serpentaire <glemoine@hotmail.fr>
2023-02-18 18:25:00 +0100

    création sidebar membre du bureau

--- frontend/src/components/sidebar/SideBarMembreEquipe.jsx
+++ frontend/src/components/sidebar/SideBarMembreEquipe.jsx
@@ -6,7 +6,7 @@ function SideBarMembreEquipe() {
       <h1 className="rounded-3xl text-1xl text-center text-green font-semibold p-3 md:text-start md:pl-14">
         Espace membre d'équipe
       </h1>
-      <div className="md:w-60 md:w-56 border pt-2 pb-2 border-black hover:scale-105 duration-300 rounded-3xl shadow-2xl lg:max-w-lg mt-3 mx-8">
+      <div className="md:w-60 md:w-56 border pt-2 pb-2 border-black hover:scale-105 duration-300 rounded-3xl shadow-lg lg:max-w-lg mt-3 mx-8">
         <div className="flex flex-col mx-3">
           <div className="flex flex-row justify-around items-center my-3">
             <button
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // import SideBarAdherent from "@components/sidebar/SideBarAdherent";
-import SideBarMembreEquipe from "@components/sidebar/SideBarMembreEquipe";
+// import SideBarMembreEquipe from "@components/sidebar/SideBarMembreEquipe";
+import SideBarMembreBureau from "@components/sidebar/SideBarMembreBureau";
 
 export default function Home() {
   return (
     <div className="footer">
       {/* <SideBarAdherent /> */}
-      <SideBarMembreEquipe />
+      {/* <SideBarMembreEquipe /> */}
+      <SideBarMembreBureau />
     </div>
   );
 }
