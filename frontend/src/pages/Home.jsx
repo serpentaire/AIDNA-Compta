6ed061bfb7fa121d7d0214d65c9fbec3eae81c9a
serpentaire <glemoine@hotmail.fr>
2023-02-18 18:51:30 +0100

    création sidebar pour la presidente la secretaire et le trésorier

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 // import SideBarAdherent from "@components/sidebar/SideBarAdherent";
 // import SideBarMembreEquipe from "@components/sidebar/SideBarMembreEquipe";
-import SideBarMembreBureau from "@components/sidebar/SideBarMembreBureau";
+// import SideBarMembreBureau from "@components/sidebar/SideBarMembreBureau";
+import SideBarTresorier from "@components/sidebar/SideBarTresorier";
 
 export default function Home() {
   return (
     <div className="footer">
       {/* <SideBarAdherent /> */}
       {/* <SideBarMembreEquipe /> */}
-      <SideBarMembreBureau />
+      {/* <SideBarMembreBureau /> */}
+      <SideBarTresorier />
     </div>
   );
 }
