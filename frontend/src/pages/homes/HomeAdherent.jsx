0f967577db168cc7560cf2256089de58d3128b3e
serpentaire <glemoine@hotmail.fr>
2023-02-25 10:48:51 +0100

    creation page home adherent

---
+++ frontend/src/pages/homes/HomeAdherent.jsx
@@ -0,0 +1,22 @@
+import SideBarAdherent from "../../components/sidebar/SideBarAdherent";
+
+export default function HomeAdherent() {
+  return (
+    <div className="homeAdherent md:flex  ">
+      <div>
+        <SideBarAdherent />
+      </div>
+      <div className=" border pt-2 pb-2 border-black rounded-3xl shadow-lg mt-3 mx-8">
+        <p className="rounded-3xl text-1xl text-center text-green font-semibold p-5">
+          Bonjour l'association AIDNA est heureuse de vous compter parmi ses
+          adhérents. En tant qu'adhèrent vous pouvez avoir accès au résultat des
+          comptes de l'association. Vous pouvez voir le bilan annuel et le bilan
+          par projet. Ceci en cliquant sur les boutons. Pour voir les comptes en
+          détails il faut en faire la demande par écrit à la présidente. Une
+          visio ou une rencontre sera organisée afin de vous montrer l'état des
+          comptes.
+        </p>
+      </div>
+    </div>
+  );
+}
