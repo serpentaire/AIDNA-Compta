a2cbbf538c7cdd9100ba689250996a6600af165a
serpentaire <glemoine@hotmail.fr>
2023-03-15 23:29:54 +0100

    mise en place post enregistrement

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,17 +1,40 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
 const nComptesControllers = require("./controllers/nComptesControllers");
 const loginControllers = require("./controllers/loginControllers");
 const banqueControllers = require("./controllers/banqueControllers");
 const modePayControllers = require("./controllers/modePayControllers");
+const compteControllers = require("./controllers/compteControllers");
 // const itemControllers = require("./controllers/itemControllers");
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename(req, file, cb) {
+    const fileArray = file.originalname.split(".");
+    const ext = fileArray.pop();
+    const filename = fileArray.join("_").split(" ").join("_");
+    cb(
+      null,
+      `${file.fieldname === "/images/"}${`${filename}_${Date.now()}.${ext}`}`
+    );
+  },
+});
+const upload = multer({ storage });
 
 // router.get("/items", itemControllers.browse);
 // router.get("/items/:id", itemControllers.read);
 // router.put("/items/:id", itemControllers.edit);
 // router.post("/items", itemControllers.add);
 // router.delete("/items/:id", itemControllers.destroy);
+router.post(
+  "/enregistrement",
+  upload.fields([{ name: "facture", maxCount: 1 }]),
+  compteControllers.add
+);
 router.get("/modePaiement", modePayControllers.browse);
 router.get("/banque", banqueControllers.browse);
 router.get("/nComptes", nComptesControllers.browse);
