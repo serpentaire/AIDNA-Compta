1f4ed6b614b3070f4e250a43ba30445f7dae51ae
serpentaire <glemoine@hotmail.fr>
2023-03-29 21:59:31 +0200

    refactorisation du CSS

--- frontend/src/components/sidebar/SideBarMembreBureau.jsx
+++ frontend/src/components/sidebar/SideBarMembreBureau.jsx
@@ -3,66 +3,44 @@ import React from "react";
 function SideBarMembreBureau() {
   return (
     <div className="sideBarMembreBureau">
-      <h1 className="rounded-3xl text-1xl text-center text-green font-semibold p-3 md:text-start md:pl-12">
+      <h1 className="h1sidebar text-1xl md:text-start md:pl-12">
         Espace membre du bureau
       </h1>
-      <div className="md:w-60 md:w-56 border pt-2 pb-2 border-black hover:scale-105 duration-300 rounded-3xl shadow-lg lg:max-w-lg mt-3 mx-8">
-        <h2 className="rounded-3xl text-1xl text-center text-green font-semibold p-3 md:text-start md:pl-20">
-          Comptes
-        </h2>
+      <div className="card">
+        <h2 className="h2sidebar text-1xl md:text-start md:pl-20">Comptes</h2>
         <div className="flex flex-col mx-3">
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-3/4 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Compte journalier
             </button>
           </div>
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-3/4 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               caisse
             </button>
           </div>
         </div>
       </div>
-      <div className="md:w-60 md:w-56 border pt-2 pb-2 border-black hover:scale-105 duration-300 rounded-3xl shadow-lg lg:max-w-lg mt-3 mx-8">
-        <h2 className="rounded-3xl text-1xl text-center text-green font-semibold p-3 md:text-start md:pl-20">
-          Rapports
-        </h2>
+      <div className="card">
+        <h2 className="h2sidebar text-1xl md:text-start md:pl-20">Rapports</h2>
         <div className="flex flex-col mx-3">
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-3/4 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Comptes
             </button>
           </div>
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-3/4 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Bilan
             </button>
           </div>
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-3/4 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Projet
             </button>
           </div>
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-3/4 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Analyse des comptes
             </button>
           </div>
--- frontend/src/components/sidebar/SideBarMembreEquipe.jsx
+++ frontend/src/components/sidebar/SideBarMembreEquipe.jsx
@@ -3,32 +3,23 @@ import React from "react";
 function SideBarMembreEquipe() {
   return (
     <div>
-      <h1 className="rounded-3xl text-1xl text-center text-green font-semibold p-3 md:text-start md:pl-14">
+      <h1 className="h1sidebar text-1xl  md:text-start md:pl-14">
         Espace membre d'équipe
       </h1>
-      <div className="md:w-60 md:w-56 border pt-2 pb-2 border-black hover:scale-105 duration-300 rounded-3xl shadow-lg lg:max-w-lg mt-3 mx-8">
+      <div className="card">
         <div className="flex flex-col mx-3">
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-1/2 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Comptes
             </button>
           </div>
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-1/2 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Bilan Annuel
             </button>
           </div>
           <div className="flex flex-row justify-around items-center my-3">
-            <button
-              className="rounded-full p-2 bg-orange text-white font-bold w-1/2 md:w-3/4"
-              type="button"
-            >
+            <button className="btnCustom btnCustumFocus" type="button">
               Bilan Projet
             </button>
           </div>
