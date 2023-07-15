84e4863b2cf8911910ca36bac60a8f5104f9eb50
serpentaire <glemoine@hotmail.fr>
2023-07-15 17:33:16 +0200

    mise en place du front

--- frontend/src/pages/homes/HomeAdherent.jsx
+++ frontend/src/pages/homes/HomeAdherent.jsx
@@ -1,6 +1,44 @@
+import React, { useState, useContext } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import apiConnexion from "../../services/apiConnexion";
+import toastiConfig from "../../services/toastiConfig";
 import SideBarAdherent from "../../components/sidebar/SideBarAdherent";
+import User from "../../context/user";
 
 export default function HomeAdherent() {
+  const { user } = useContext(User.UserContext);
+  const contactType = {
+    nom: user[0].nom,
+    prenom: user[0].prenom,
+    email: user[0].Users_log.login,
+    message: "",
+  };
+  // console.log(user);
+
+  const [contact, setContact] = useState(contactType);
+  // console.log(contact);
+  const handelContact = (place, value) => {
+    const newContact = { ...contact };
+    newContact[place] = value;
+    setContact(newContact);
+  };
+
+  const sendForm = (e) => {
+    e.preventDefault();
+    apiConnexion
+      .post("/contact", contact)
+      .then(() => {
+        toast.success(
+          "Bonjour, votre message a bien été envoyé.",
+          toastiConfig
+        );
+      })
+      .catch(() => {
+        toast.error("Une erreur est survenue", toastiConfig);
+        console.warn();
+      });
+  };
   return (
     <div className="homeAdherent md:flex ">
       <div>
@@ -12,11 +50,67 @@ export default function HomeAdherent() {
           adhérents. En tant qu'adhèrent vous pouvez avoir accès au résultat des
           comptes de l'association. Vous pouvez voir le bilan annuel et le bilan
           par projet. Ceci en cliquant sur les boutons. Pour voir les comptes en
-          détails il faut en faire la demande par écrit à la présidente. Une
-          visio ou une rencontre sera organisée afin de vous montrer l'état des
-          comptes.
+          détails il faut en faire la demande par écrit à la présidente à l'aide
+          du formulaire de contact. Une visio ou une rencontre sera organisée
+          afin de vous montrer l'état des comptes.
         </p>
+        <form onSubmit={(e) => sendForm(e)}>
+          <div className=" divEnCustom">
+            <h2 className="h2compo grow ">Nom</h2>
+            <input
+              required
+              className="inputCustom md:w-1/4"
+              type="text"
+              name="nom"
+              value={contact.nom}
+              onChange={(e) => handelContact(e.target.name, e.target.value)}
+            />
+          </div>
+          <div className=" divEnCustom">
+            <h2 className="h2compo grow">Prénom</h2>
+            <input
+              required
+              className="inputCustom md:w-1/4"
+              type="text"
+              name="nom"
+              value={contact.prenom}
+              onChange={(e) => handelContact(e.target.name, e.target.value)}
+            />
+          </div>
+          <div className=" divEnCustom">
+            <h2 className="h2compo grow">Message :</h2>
+            <textarea
+              required
+              className="inputCustom md:w-3/4 rounded-lg"
+              type="text"
+              name="message"
+              placeholder="Message"
+              value={contact.message}
+              onChange={(e) => handelContact(e.target.name, e.target.value)}
+            />
+          </div>
+          <div className="flex flex-row justify-around items-center my-3 md:justify-end md:pr-20">
+            <button
+              className="btnCustom focus:btnCustumFocus justify-center m-2 md:w-40"
+              type="submit"
+            >
+              Envoyer
+            </button>
+          </div>
+        </form>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
