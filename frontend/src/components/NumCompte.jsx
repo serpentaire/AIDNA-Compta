7e85d3399237d8a4ca27fc49515b26c0a95c744d
serpentaire <glemoine@hotmail.fr>
2023-09-13 19:26:41 +0200

    mise en place test front composant NumCompte

--- frontend/src/components/NumCompte.jsx
+++ frontend/src/components/NumCompte.jsx
@@ -21,14 +21,30 @@ function NumCompte() {
     <div className="numCompte">
       <div>
         <h1 className="h1compo">Liste des comptes</h1>
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="1" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="2" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="3" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="4" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="5" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="6" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="7" />
-        <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="8" />
+        <div data-testid="TableauCompte1">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="1" />
+        </div>
+        <div data-testid="TableauCompte2">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="2" />
+        </div>
+        <div data-testid="TableauCompte3">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="3" />
+        </div>
+        <div data-testid="TableauCompte4">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="4" />
+        </div>
+        <div data-testid="TableauCompte5">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="5" />
+        </div>
+        <div data-testid="TableauCompte6">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="6" />
+        </div>
+        <div data-testid="TableauCompte7">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="7" />
+        </div>
+        <div data-testid="TableauCompte8">
+          <TableauCompte nCompte={nCompte} updateChild={getNcompte} nb="8" />
+        </div>
       </div>
     </div>
   );
